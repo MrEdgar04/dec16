e3dfcef089f34e1d3efe13d77f01e6ef2606afd0
Edgar Stark <E.stark@donboscosandona.it>
2021-12-16 09:08:19 +0000

    app base

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+
+    #questa sarò la home page
+    def index 
+        
+    end
+
+    #questa sarà la pagina chi siamo
+    def chi_siamodule 
+
+    end
+
+end
